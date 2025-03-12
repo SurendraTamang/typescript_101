218d1517396d081bf1eb925348a3bfb16b68c88f
surendra <tamangsurendra44@gmail.com>
2025-03-12 04:20:07 +0000

    Teaching basics

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,6 +1,26 @@
 console.log("Learning TypesScript Again.")
-
+// primitive types
 let a: number = 4
+console.log(a, typeof a)
 let b: string = "Surendra"
-let d: number = a + b
-// Above shows the error while writing 
+console.log(b, typeof b)
+let d: number | string = a + b // Union
+console.log(d, typeof d)
+let isTypeScriptGood: boolean = true
+console.log(isTypeScriptGood, typeof isTypeScriptGood)
+let mySalary : bigint = 1000000000000000000n
+console.log(mySalary, typeof mySalary)
+let x : Symbol = Symbol('x')
+console.log(x, typeof x)
+
+// builtin types
+type Age = number
+
+let age : Age = 30
+
+// 
+
+
+
+
+ 
